a1c0b1613f3e0f63eae52bb2329eef9b6a2f22df
saugatmalla <saugat013@gmail.com>
2020-10-07 16:41:53 -0400

    @hapi/joi validation pacakage checkpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,36 @@
 const router = require('express').Router();
-const mongoose = require('mongoose');
-require('dotenv/config')
+const User = require('../models/User');
 
-//connect to db
-mongoose.connect(process.env.MONGODB,
-    {useNewUrlParser: true, useUnifiedTopology: true}, function() {
-        console.log('connected to db')
+//Validation
+
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
+router.post('/register', async (req, res) => {
+
+    const {error} = schema.validate(req.body);
+    if(error) {
+        return res.send(error.details[0].message)
+    }
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     })
 
-router.post('/register', (req, res) => {
-    res.send('register')
+    try {
+        const savedUser = await user.save()
+        res.send(savedUser)
+        console.log(savedUser)
+    } catch (error) {
+        res.send(error)
+    }
+
 })
 
 router.post('/login', (req, res) => {
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 //Routes
 const authRoute = require('./routes/auth');
 
+//connect to db
+mongoose.connect(process.env.MONGODB,
+    { useNewUrlParser: true, useUnifiedTopology: true }, function () {
+        console.log('connected to db')
+    })
+    
+//Middlewares
+app.use(express.json())
+
+//route middlewares
 app.use('/api/user', authRoute);
+
 
 app.listen(3000, () => {
     console.log('server running in port 3000')
